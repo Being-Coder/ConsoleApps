24953d4f58b037e57a3efd6a30df1fb31ad4e562
Being-Coder <aditya221127@gmail.com>
2023-10-31 14:14:44 +0530

    Console check-in

---
+++ ExtenMthd-1.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+
+//namespace MyProject;
+
+//public class CtrDtrClass
+//{
+//    static void Main(string[] args)
+//    {
+//        List<int> length = new List<int>() { 1, 2, 3, 4, 5 };
+//        length.Print();
+
+//        string str = "pragim";
+//        str = str.ToFirstUpper();
+//        Console.WriteLine(str);
+//    }
+//}
+
+//public static class PrintExtension
+//{ 
+//    public static string ToFirstUpper(this string str)
+//    {
+//        char[] chars = str.ToCharArray();
+//        chars[0] = char.ToUpper(chars[0]);
+//        return str = new string(chars);
+//    }
+//    public static void Print(this List<int> list)
+//    {
+//        foreach (int i in list)
+//        {
+//            Console.WriteLine(i);
+//        }
+//    }
+//}
---
+++ WrdFreqCnt.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+namespace MyProject;
+public class CtrDtrClass
+{
+    static void Main(string[] args)
+    {
+
+        //[1:38 PM] Vishal Waman
+        //Task: Word Frequency Counter
+
+        //Create a C# console application that reads a text file and counts the
+        //frequency of each word in the file. The application should display the
+        //top N most frequent words.
+
+
+        string str = "the quick brown fox jumps over the lazy dog and the cat";
+        ABC aBC = new ABC();
+        aBC.FreCounter(str);
+
+    }
+}
+
+public class ABC
+{
+    public void FreCounter(string str)
+    {
+        Dictionary<char,int> dic = new Dictionary<char,int>();
+
+        foreach (char c in str)
+        {
+            int freq = 1;
+
+            if (dic.ContainsKey(c))
+            {
+                dic[c] = freq + 1;
+            }
+            else
+            {
+                dic.Add(c,freq);
+            }
+        }
+
+        foreach(KeyValuePair<char,int> kvp in dic)
+        {
+            Console.WriteLine("Key : {0} & Value : {1}", kvp.Key, kvp.Value);
+        }
+    }
+}
+
+
+
